9e191544188f10d7a341d510933ffd20587fe795
DgrinderHZ <hassanzekkouri@gmail.com>
2022-07-06 17:59:35 +0100

    Request body

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from models import Course
 
 app = FastAPI()
 
@@ -12,11 +16,17 @@ def main():
 def read_course(course_name):
     return {'course_name': course_name}
 
-@app.get('/courses/{course_id}')
-def read_course(course_id: int):
-    return {'course_id': course_id}
+@app.get('/courses_/{course_id}')
+def read_course(course_id: int, q: Optional[str] = None):
+    if q is not None:
+        return {"course_name": course_items[course_id], "q": q} 
+    return {"course_name": course_items[course_id]}
 
 # QUERY PARAMS
 @app.get('/courses/')
-def read_courses(start: int, end: int):
+def read_courses(start: int = 0, end: int = 10):
     return course_items[start : start + end]
+
+@app.post('/create')
+def create(course: Course):
+    return course
